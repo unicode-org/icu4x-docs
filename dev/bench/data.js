a87cd1b72300abe1d96fbf1b365480142eda5ff8
zbraniecki <zbraniecki@users.noreply.github.com>
2020-09-21 20:56:12 +0000

    Bench Dash (runtime, uniset) -> GH Pages unicode-org/icu4x@b3891597b1e4e0e9e78a1679f0596229957a173b

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1600721770841,
+  "repoUrl": "https://github.com/unicode-org/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b3891597b1e4e0e9e78a1679f0596229957a173b",
+          "message": "Add PluralRules example (#249)\n\n* Add PluralRules example\r\n\r\n* Apply feedback",
+          "timestamp": "2020-09-21T13:52:22-07:00",
+          "tree_id": "69eedd85c39c57e56d3a75a076149489950214da",
+          "url": "https://github.com/unicode-org/icu4x/commit/b3891597b1e4e0e9e78a1679f0596229957a173b"
+        },
+        "date": 1600721770185,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/contains/best",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains/worst",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains_range/best",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains_range/worst",
+            "value": 10,
+            "range": "± 1",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}

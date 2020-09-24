89fb2fbd8fd4f746d5468e4f5036513d18b46541
filmil <filmil@users.noreply.github.com>
2020-09-24 17:22:41 +0000

    Rust doc -> GH Pages unicode-org/icu4x@f49fdbece75590675fb1c70057c7257251e2fd2e

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["icu_fs_data_provider"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"icu_fs_data_provider/export/fs_exporter/struct.FilesystemExporter.html\" title=\"struct icu_fs_data_provider::export::fs_exporter::FilesystemExporter\">FilesystemExporter</a>","synthetic":false,"types":["icu_fs_data_provider::export::fs_exporter::FilesystemExporter"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

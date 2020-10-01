3a151f73230238fa378f5800e444ef7cb7b24d42
filmil <filmil@users.noreply.github.com>
2020-10-01 20:30:59 +0000

    Bench Dash (runtime, uniset) -> GH Pages unicode-org/icu4x@bd4d2a5b7057fed49717780872cbe5fb7d137d4a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1601534659551,
+  "lastUpdate": 1601584258621,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -16,127 +16,37 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "e6579f8c6342fe323641ffb665d3829c0f581d2a",
-          "message": "Makes `PluralOperands::n` into a getter (#284)\n\n* Makes `PluralOperands::n` into a getter\r\n\r\nThe field `n` is not used for plural selection, and may cause\r\nprecision loss in cases of conversion.  So we stop keeping in\r\naround and compute it on demand instead.\r\n\r\nFixes #283\r\n\r\n* fixup: simplify eval logic, clean up lint",
-          "timestamp": "2020-09-30T23:35:42-07:00",
-          "tree_id": "e83a81118db0d8fd82ee8e57e93dffc37c2a5875",
-          "url": "https://github.com/unicode-org/icu4x/commit/e6579f8c6342fe323641ffb665d3829c0f581d2a"
+          "id": "bd4d2a5b7057fed49717780872cbe5fb7d137d4a",
+          "message": "Implements From<FixedDecimal> for PluralOperands (#278)\n\n* Implements From<FixedDecimal> for PluralOperands\r\n\r\nThis will allow us to convert FixedDecimal representation without loss\r\nof precision into PluralOperands, for correct plural selection.\r\n\r\nFor example, a number `25` may sometimes be pluralized differently from\r\n`25.0`.\r\n\r\nAdded the benchmarks for the conversion, though without a baseline it is\r\nnot just as useful yet.  (Until we try to reimplement.)\r\n\r\nSee issue #190.\r\n\r\n* Rewrite eq() to branchless\r\n\r\n* Adds a test for eq()\r\n\r\nWe needed to add a custom implementation for eq() to account\r\nfor loss of precision in PluralOperands.\r\n\r\n* fixup:first\r\n\r\n* Clean up the code for From\r\n\r\n* Adds more illustrative naming in From\r\n\r\n* Makes clippy happy\r\n\r\n* Pulls num_fractional_digits out of the loop\r\n\r\nIt is possible to compute it based on the low end of the magnitude\r\nrange.  No performance change per benchmark.\r\n\r\n* fixup: benchmark was wrong\r\n\r\n* fixup: moves new operand tests to json\r\n\r\nDefines new tests for the data model for conversion, and places them\r\ninto the JSON files instead of inline with the tests.\r\n\r\n* fixup: moves the benchmark loop in\r\n\r\nThis allows criterion to run the benchmark loop\r\nwith a specific time limit.\r\n\r\n* fixup: formatting\r\n\r\n* fixup: adds individual sample measurements\r\n\r\nHelps smoke out specific performance regressions.",
+          "timestamp": "2020-10-01T13:26:52-07:00",
+          "tree_id": "cca4272fa893e7481952a652fd042f3698158b81",
+          "url": "https://github.com/unicode-org/icu4x/commit/bd4d2a5b7057fed49717780872cbe5fb7d137d4a"
         },
-        "date": 1601534658834,
+        "date": 1601584258222,
         "tool": "cargo",
         "benches": [
           {
-            "name": "langid/construct/langid",
-            "value": 721,
-            "range": "± 40",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/construct/locale",
-            "value": 1235,
-            "range": "± 71",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/to_string/langid",
-            "value": 2215,
-            "range": "± 133",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/to_string/locale",
-            "value": 2157,
-            "range": "± 125",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/compare/struct/langid",
-            "value": 16,
-            "range": "± 1",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/compare/struct/locale",
-            "value": 16,
+            "name": "uniset/contains/best",
+            "value": 10,
             "range": "± 0",
             "unit": "ns/iter"
           },
           {
-            "name": "langid/compare/str/langid",
-            "value": 2361,
-            "range": "± 131",
+            "name": "uniset/contains/worst",
+            "value": 10,
+            "range": "± 0",
             "unit": "ns/iter"
           },
           {
-            "name": "langid/compare/str/locale",
-            "value": 2483,
-            "range": "± 258",
+            "name": "uniset/contains_range/best",
+            "value": 10,
+            "range": "± 1",
             "unit": "ns/iter"
           },
           {
-            "name": "langid/canonicalize/langid",
-            "value": 3521,
-            "range": "± 225",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/canonicalize/locale",
-            "value": 5469,
-            "range": "± 323",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/construct/locale",
-            "value": 2584,
-            "range": "± 171",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/to_string/locale",
-            "value": 3486,
-            "range": "± 216",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/compare/struct/locale",
-            "value": 117,
-            "range": "± 8",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/compare/str/locale",
-            "value": 3513,
-            "range": "± 178",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/canonicalize/locale",
-            "value": 7059,
-            "range": "± 333",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "language_subtag_parse",
-            "value": 343,
-            "range": "± 30",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "script_subtag_parse",
-            "value": 125,
-            "range": "± 7",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "region_subtag_parse",
-            "value": 132,
-            "range": "± 5",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "variant_subtag_parse",
-            "value": 267,
-            "range": "± 16",
+            "name": "uniset/contains_range/worst",
+            "value": 9,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]

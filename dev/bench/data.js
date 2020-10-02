b04059509d30c06a772d658060dc4fdcb4260df9
zbraniecki <zbraniecki@users.noreply.github.com>
2020-10-02 15:48:08 +0000

    Bench Dash (runtime, uniset) -> GH Pages unicode-org/icu4x@608d8bb67494eb092cc31a1a16799c8924348787

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1601613929621,
+  "lastUpdate": 1601653687974,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -16,127 +16,37 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "8ddae047ea363f6752d0d7b996f71139a10f6c93",
-          "message": "Scope out subbenchmarks to optional features (#286)",
-          "timestamp": "2020-10-01T21:37:27-07:00",
-          "tree_id": "5ee69cf09d17353894e316262bc5e06432fd0051",
-          "url": "https://github.com/unicode-org/icu4x/commit/8ddae047ea363f6752d0d7b996f71139a10f6c93"
+          "id": "608d8bb67494eb092cc31a1a16799c8924348787",
+          "message": "Add DateTimeFormat (#202)\n\n* Add DateTimeFormat\r\n\r\n* Add Preferences to Options and remove leftovers from Skeleton building\r\n\r\n* Add fixtures and data driven tests\r\n\r\n* Add docs and clean up the code\r\n\r\n* Clippy!\r\n\r\n* More clippy\r\n\r\n* More clippy\r\n\r\n* Remove some monads that are not needed anymore and remove all unwraps from src\r\n\r\n* Apply reviewers feedback\r\n\r\n* Apply reviewers feedback\r\n\r\n* Improve format_number to comply with UTS 35 and fix clippy\r\n\r\n* Improve pattern parsing\r\n\r\n* Switch parser pattern to Bytes\r\n\r\n* Apply feedback\r\n\r\n* Add an example",
+          "timestamp": "2020-10-02T08:43:43-07:00",
+          "tree_id": "5c46735e9831b6ac842fa35394e9977ee0c59005",
+          "url": "https://github.com/unicode-org/icu4x/commit/608d8bb67494eb092cc31a1a16799c8924348787"
         },
-        "date": 1601613929213,
+        "date": 1601653687600,
         "tool": "cargo",
         "benches": [
           {
-            "name": "langid/construct/langid",
-            "value": 738,
-            "range": "± 5",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/construct/locale",
-            "value": 1186,
-            "range": "± 9",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/to_string/langid",
-            "value": 2071,
-            "range": "± 20",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/to_string/locale",
-            "value": 2122,
-            "range": "± 294",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/compare/struct/langid",
-            "value": 18,
-            "range": "± 1",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/compare/struct/locale",
-            "value": 18,
+            "name": "uniset/contains/best",
+            "value": 11,
             "range": "± 0",
             "unit": "ns/iter"
           },
           {
-            "name": "langid/compare/str/langid",
-            "value": 2349,
-            "range": "± 83",
+            "name": "uniset/contains/worst",
+            "value": 11,
+            "range": "± 0",
             "unit": "ns/iter"
           },
           {
-            "name": "langid/compare/str/locale",
-            "value": 2333,
-            "range": "± 99",
+            "name": "uniset/contains_range/best",
+            "value": 10,
+            "range": "± 0",
             "unit": "ns/iter"
           },
           {
-            "name": "langid/canonicalize/langid",
-            "value": 3542,
-            "range": "± 104",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/canonicalize/locale",
-            "value": 5276,
-            "range": "± 214",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/construct/locale",
-            "value": 2451,
-            "range": "± 96",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/to_string/locale",
-            "value": 3247,
-            "range": "± 216",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/compare/struct/locale",
-            "value": 141,
-            "range": "± 5",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/compare/str/locale",
-            "value": 3317,
-            "range": "± 255",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/canonicalize/locale",
-            "value": 6669,
-            "range": "± 376",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "language_subtag_parse",
-            "value": 343,
-            "range": "± 13",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "script_subtag_parse",
-            "value": 120,
-            "range": "± 2",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "region_subtag_parse",
-            "value": 122,
-            "range": "± 4",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "variant_subtag_parse",
-            "value": 270,
-            "range": "± 8",
+            "name": "uniset/contains_range/worst",
+            "value": 11,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]

c918d69b8061470b11cd5e5a9361c48a17103ed8
sffc <sffc@users.noreply.github.com>
2020-10-03 05:16:58 +0000

    Bench Dash (runtime, num-util) -> GH Pages unicode-org/icu4x@d3a6cd1651e9720fbd883cfa44d78906586213ad

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1601702064041,
+  "lastUpdate": 1601702217043,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22,30 +22,36 @@ window.BENCHMARK_DATA = {
           "tree_id": "58b364f428c486bc366cd4364fa31063b2899079",
           "url": "https://github.com/unicode-org/icu4x/commit/d3a6cd1651e9720fbd883cfa44d78906586213ad"
         },
-        "date": 1601702063349,
+        "date": 1601702216605,
         "tool": "cargo",
         "benches": [
           {
-            "name": "uniset/contains/best",
-            "value": 9,
+            "name": "isize/smaller",
+            "value": 34838,
+            "range": "± 1646",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 82711,
+            "range": "± 153",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/908070605040302010",
+            "value": 308,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write_to/908070605040302010",
+            "value": 145,
             "range": "± 0",
             "unit": "ns/iter"
           },
           {
-            "name": "uniset/contains/worst",
-            "value": 9,
-            "range": "± 0",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "uniset/contains_range/best",
-            "value": 10,
-            "range": "± 0",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "uniset/contains_range/worst",
-            "value": 9,
+            "name": "from_string/1000000001",
+            "value": 94,
             "range": "± 0",
             "unit": "ns/iter"
           }

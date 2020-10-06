6b3259781b4a09939208e462a529a63050877d30
sffc <sffc@users.noreply.github.com>
2020-10-06 14:05:23 +0000

    Bench Dash (runtime, uniset) -> GH Pages unicode-org/icu4x@b100859a1e2ff1b89193c2cdbb484c199c56b591

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,14 +1,14 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1601931429452,
+  "lastUpdate": 1601993122448,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
       {
         "commit": {
           "author": {
-            "email": "zibi@braniecki.net",
-            "name": "Zibi Braniecki",
-            "username": "zbraniecki"
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
           },
           "committer": {
             "email": "noreply@github.com",
@@ -16,127 +16,37 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "554b3b5669a54b7694ceea49730e71bef4c5998d",
-          "message": "Reorganize PluralRules benchmarks (#295)",
-          "timestamp": "2020-10-05T13:49:29-07:00",
-          "tree_id": "6ae9a99fa1b888b3cb8e41b674d3c99cff4a5698",
-          "url": "https://github.com/unicode-org/icu4x/commit/554b3b5669a54b7694ceea49730e71bef4c5998d"
+          "id": "b100859a1e2ff1b89193c2cdbb484c199c56b591",
+          "message": "Limit magnitudes of From<FixedDecimal> for PluralOperands to prevent overflow. (#293)",
+          "timestamp": "2020-10-06T09:00:53-05:00",
+          "tree_id": "c35333acdb2bc40b4c33803746a627f1be729b44",
+          "url": "https://github.com/unicode-org/icu4x/commit/b100859a1e2ff1b89193c2cdbb484c199c56b591"
         },
-        "date": 1601931429087,
+        "date": 1601993121813,
         "tool": "cargo",
         "benches": [
           {
-            "name": "langid/construct/langid",
-            "value": 700,
-            "range": "± 15",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/construct/locale",
-            "value": 1147,
-            "range": "± 19",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/to_string/langid",
-            "value": 1922,
-            "range": "± 59",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/to_string/locale",
-            "value": 1950,
-            "range": "± 45",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/compare/struct/langid",
-            "value": 16,
+            "name": "uniset/contains/best",
+            "value": 11,
             "range": "± 0",
             "unit": "ns/iter"
           },
           {
-            "name": "langid/compare/struct/locale",
-            "value": 15,
+            "name": "uniset/contains/worst",
+            "value": 11,
             "range": "± 0",
             "unit": "ns/iter"
           },
           {
-            "name": "langid/compare/str/langid",
-            "value": 2174,
-            "range": "± 79",
+            "name": "uniset/contains_range/best",
+            "value": 11,
+            "range": "± 0",
             "unit": "ns/iter"
           },
           {
-            "name": "langid/compare/str/locale",
-            "value": 2078,
-            "range": "± 134",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/canonicalize/langid",
-            "value": 3168,
-            "range": "± 121",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "langid/canonicalize/locale",
-            "value": 5023,
-            "range": "± 172",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/construct/locale",
-            "value": 2158,
-            "range": "± 102",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/to_string/locale",
-            "value": 2762,
-            "range": "± 99",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/compare/struct/locale",
-            "value": 126,
-            "range": "± 4",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/compare/str/locale",
-            "value": 3046,
-            "range": "± 79",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "locale/canonicalize/locale",
-            "value": 6083,
-            "range": "± 203",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "language_subtag_parse",
-            "value": 310,
-            "range": "± 15",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "script_subtag_parse",
-            "value": 94,
-            "range": "± 4",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "region_subtag_parse",
-            "value": 103,
-            "range": "± 5",
-            "unit": "ns/iter"
-          },
-          {
-            "name": "variant_subtag_parse",
-            "value": 241,
-            "range": "± 10",
+            "name": "uniset/contains_range/worst",
+            "value": 11,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]

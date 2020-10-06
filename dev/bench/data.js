6fee749b6fafeb34f826c254036783c460d70878
zbraniecki <zbraniecki@users.noreply.github.com>
2020-10-06 19:14:52 +0000

    Bench Dash (runtime, utils/fixed-decimal) -> GH Pages unicode-org/icu4x@6d6bcea21678159d29f26c5bd02bb9ce420001b4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602011544401,
+  "lastUpdate": 1602011691699,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22,31 +22,37 @@ window.BENCHMARK_DATA = {
           "tree_id": "36cc98d8a7b2070c26e35fe8b3f68d59f10d2a09",
           "url": "https://github.com/unicode-org/icu4x/commit/6d6bcea21678159d29f26c5bd02bb9ce420001b4"
         },
-        "date": 1602011543729,
+        "date": 1602011690966,
         "tool": "cargo",
         "benches": [
           {
-            "name": "uniset/contains/best",
-            "value": 11,
-            "range": "± 1",
+            "name": "isize/smaller",
+            "value": 34223,
+            "range": "± 2946",
             "unit": "ns/iter"
           },
           {
-            "name": "uniset/contains/worst",
-            "value": 9,
-            "range": "± 1",
+            "name": "isize/larger",
+            "value": 77522,
+            "range": "± 4712",
             "unit": "ns/iter"
           },
           {
-            "name": "uniset/contains_range/best",
-            "value": 10,
-            "range": "± 1",
+            "name": "to_string/908070605040302010",
+            "value": 300,
+            "range": "± 20",
             "unit": "ns/iter"
           },
           {
-            "name": "uniset/contains_range/worst",
-            "value": 9,
-            "range": "± 0",
+            "name": "write_to/908070605040302010",
+            "value": 141,
+            "range": "± 9",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 88,
+            "range": "± 5",
             "unit": "ns/iter"
           }
         ]

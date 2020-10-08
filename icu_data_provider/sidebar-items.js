3efa27682cc9949c27736ecaf613216dbf50fa87
zbraniecki <zbraniecki@users.noreply.github.com>
2020-10-08 20:42:03 +0000

    Rust doc -> GH Pages unicode-org/icu4x@1eae2560a2b48329aa5b77968e27b91191ae9183

---
+++ icu_data_provider/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"macro":[["icu_data_key","Shortcut to construct a data key from a URI-like syntax."]],"mod":[["iter",""],["prelude",""],["structs",""]],"struct":[["InvariantDataProvider","A locale-invariant data provider. Sometimes useful for testing. Not intended to be used in production environments."]]});

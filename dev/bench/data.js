f239494f47c485acf047812cdfebd323a663a1fa
sffc <sffc@users.noreply.github.com>
2020-10-09 17:34:33 +0000

    Bench Dash (runtime, components/uniset) -> GH Pages unicode-org/icu4x@e0e3dec2353613dc1e41fcb925494b905f64ecbd

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1602264870569,
+  "repoUrl": "https://github.com/unicode-org/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e0e3dec2353613dc1e41fcb925494b905f64ecbd",
+          "message": "Add Unicode Set example (#302)",
+          "timestamp": "2020-10-09T12:30:31-05:00",
+          "tree_id": "818d2b58edadce125b1e8f540b1100bcfa454e6e",
+          "url": "https://github.com/unicode-org/icu4x/commit/e0e3dec2353613dc1e41fcb925494b905f64ecbd"
+        },
+        "date": 1602264868530,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/contains/best",
+            "value": 10,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains/worst",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains_range/best",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains_range/worst",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}

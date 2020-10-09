531f80583395e2437e3b140407781b30516071f1
sffc <sffc@users.noreply.github.com>
2020-10-09 17:51:30 +0000

    Bench Dash (runtime, utils/fixed-decimal) -> GH Pages unicode-org/icu4x@0f7661f692fb6acea4363e4159032497323ed8dd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602265729540,
+  "lastUpdate": 1602265889423,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22,31 +22,37 @@ window.BENCHMARK_DATA = {
           "tree_id": "99223729152d0ae5239e352cf3d5267a7bc08929",
           "url": "https://github.com/unicode-org/icu4x/commit/0f7661f692fb6acea4363e4159032497323ed8dd"
         },
-        "date": 1602265728761,
+        "date": 1602265888726,
         "tool": "cargo",
         "benches": [
           {
-            "name": "uniset/contains/best",
-            "value": 11,
-            "range": "± 0",
+            "name": "isize/smaller",
+            "value": 32089,
+            "range": "± 1824",
             "unit": "ns/iter"
           },
           {
-            "name": "uniset/contains/worst",
-            "value": 11,
-            "range": "± 0",
+            "name": "isize/larger",
+            "value": 78213,
+            "range": "± 6489",
             "unit": "ns/iter"
           },
           {
-            "name": "uniset/contains_range/best",
-            "value": 11,
-            "range": "± 0",
+            "name": "to_string/908070605040302010",
+            "value": 295,
+            "range": "± 24",
             "unit": "ns/iter"
           },
           {
-            "name": "uniset/contains_range/worst",
-            "value": 12,
-            "range": "± 0",
+            "name": "write_to/908070605040302010",
+            "value": 136,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 88,
+            "range": "± 7",
             "unit": "ns/iter"
           }
         ]

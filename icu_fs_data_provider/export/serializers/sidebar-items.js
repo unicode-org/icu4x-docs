3ce7c6b240e3d59a666626d3fad30c337b199cf5
sffc <sffc@users.noreply.github.com>
2020-10-09 17:53:06 +0000

    Rust doc -> GH Pages unicode-org/icu4x@0f7661f692fb6acea4363e4159032497323ed8dd

---
+++ icu_fs_data_provider/export/serializers/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Error","An Error type specifically for the Serializer that doesn't carry filenames"],["StyleOption",""]],"struct":[["JsonSerializer","A serializer for JavaScript Object Notation (JSON)."],["JsonSerializerOptions","Options bag for initializing a JsonSerializer."]],"trait":[["Serializer","A simple serializer trait that works on whole objects."]]});

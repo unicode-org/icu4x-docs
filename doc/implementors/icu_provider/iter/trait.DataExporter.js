0068732b8df19e9a75e765a85933e249475ddc80
sffc <sffc@users.noreply.github.com>
2020-10-13 23:51:59 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@764c9695a018fdfba767c03d0a8426384b93ba1b

---
+++ doc/implementors/icu_provider/iter/trait.DataExporter.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["icu_provider_fs"] = [{"text":"impl <a class=\"trait\" href=\"icu_provider/iter/trait.DataExporter.html\" title=\"trait icu_provider::iter::DataExporter\">DataExporter</a> for <a class=\"struct\" href=\"icu_provider_fs/export/fs_exporter/struct.FilesystemExporter.html\" title=\"struct icu_provider_fs::export::fs_exporter::FilesystemExporter\">FilesystemExporter</a>","synthetic":false,"types":["icu_provider_fs::export::fs_exporter::FilesystemExporter"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

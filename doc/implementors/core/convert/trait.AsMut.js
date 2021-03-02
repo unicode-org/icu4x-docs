4e9f087a4300e0e74c8c0c8f98300a55c6da39e6
dminor <dminor@users.noreply.github.com>
2021-03-02 12:17:09 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@b15c0b36901095406a67e3620a3d1d0e294b7692

---
+++ doc/implementors/core/convert/trait.AsMut.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["icu_locid"] = [{"text":"impl AsMut&lt;LanguageIdentifier&gt; for LanguageIdentifier","synthetic":false,"types":[]},{"text":"impl AsMut&lt;LanguageIdentifier&gt; for Locale","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ doc/implementors/core/convert/trait.AsRef.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["icu_locid"] = [{"text":"impl AsRef&lt;LanguageIdentifier&gt; for LanguageIdentifier","synthetic":false,"types":[]},{"text":"impl AsRef&lt;LanguageIdentifier&gt; for Locale","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

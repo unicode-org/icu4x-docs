330bcbbd2d71292e33d6ccc3363df78335fff43a
Manishearth <Manishearth@users.noreply.github.com>
2021-07-13 18:39:41 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@56c71b72ce26499abf258e877fb7bfe61bc274c4

---
+++ doc/zerovec/samples/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["BINCODE_BUF","Representation of TEST_SLICE in Bincode."],["JSON_STR","Representation of TEST_SLICE in JSON."],["TEST_BUFFER_LE","An example byte array intended to be used in `ZeroVec<u32>`."],["TEST_SLICE","u32 numbers corresponding to the above byte array."],["TEST_SUM","The sum of the numbers in TEST_SLICE."]]});

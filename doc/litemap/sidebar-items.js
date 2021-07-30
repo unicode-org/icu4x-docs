359ce002dbe6a80e7bfb00d8a26b9caf28f84cbf
sffc <sffc@users.noreply.github.com>
2021-07-30 00:13:51 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@d3a5153c6da23e434a05ddfae441d2ecc39f1d5c

--- doc/litemap/sidebar-items.js
+++ doc/litemap/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["LiteMap","A simple \"flat\" map based on a sorted vector"]]});
+initSidebarItems({"struct":[["LiteMap","A simple “flat” map based on a sorted vector"]]});

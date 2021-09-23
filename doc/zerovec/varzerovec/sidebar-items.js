ff98b6960ffbb5ee972bc9148daa91f84b85fa89
Manishearth <Manishearth@users.noreply.github.com>
2021-09-23 02:09:45 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@877707aa532a45ec51c1f2a27140ce632ed6173d

---
+++ doc/zerovec/varzerovec/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["VarZeroVecError",""]],"struct":[["VarZeroVec","A zero-copy vector for variable-width types."],["VarZeroVecULE","A VarULE version of VarZeroVec allowing for VarZeroVecs to be nested indefinitely"]],"type":[["ParseErrorFor",""]]});

717ab037a62bfad561df0fb071eed05ec3a090e7
sffc <sffc@users.noreply.github.com>
2021-09-23 15:10:00 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@ea4b7d9d7b2363dbcfd5d883e9890fe881ddea45

--- doc/icu_provider_blob/sidebar-items.js
+++ doc/icu_provider_blob/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["export","Utilities for dumping data to an ICU4X filesystem tree."]],"struct":[["StaticDataProvider","A data provider loading data statically baked in to the binary."]]});
+initSidebarItems({"mod":[["export","Utilities for dumping data to an ICU4X filesystem tree."]],"struct":[["BlobDataProvider","A data provider loading data from blobs dynamically created at runtime."],["StaticDataProvider","A data provider loading data statically baked in to the binary."]]});

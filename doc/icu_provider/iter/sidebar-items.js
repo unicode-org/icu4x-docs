e66f7aa0bcde8aac02450c30ca63814231a74bba
robertbastian <robertbastian@users.noreply.github.com>
2022-02-10 09:29:14 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@bb0e04733217efeaf12247ad0282bd794315ca0d

--- doc/icu_provider/iter/sidebar-items.js
+++ doc/icu_provider/iter/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["IterableProvider","A provider that can iterate over all supported [`ResourceOptions`] for a certain key."]]});
+initSidebarItems({"trait":[["IterableDynProvider","A [`DynProvider`] that can iterate over all supported [`ResourceOptions`] for a certain key."],["IterableResourceProvider","A [`ResourceProvider`] that can iterate over all supported [`ResourceOptions`] for a certain key."]]});

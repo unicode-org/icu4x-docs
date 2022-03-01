6b86810ef0cc7f93a1c117a74e469e4adf38d4c8
Manishearth <Manishearth@users.noreply.github.com>
2022-03-01 01:08:13 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@dc36207f4f9e774a014fb1a692b4bf1726f7df49

--- doc/implementors/num_enum/trait.TryFromPrimitive.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["icu_properties"] = [{"text":"impl TryFromPrimitive for <a class=\"enum\" href=\"icu_properties/enum.GeneralCategory.html\" title=\"enum icu_properties::GeneralCategory\">GeneralCategory</a>","synthetic":false,"types":["icu_properties::props::GeneralCategory"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

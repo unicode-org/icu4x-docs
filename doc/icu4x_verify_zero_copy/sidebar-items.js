26f76759d4d39be614b462c4a396eca72d709904
Manishearth <Manishearth@users.noreply.github.com>
2022-03-14 17:49:34 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@e8562c1b68cff0ebb4a85b86c2c716ddb63cd3e6

--- doc/icu4x_verify_zero_copy/sidebar-items.js
+++ doc/icu4x_verify_zero_copy/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["main",""]],"static":[["ALLOC",""],["EXPECTED_VIOLATIONS",""]]});
+initSidebarItems({"fn":[["main",""]],"static":[["ALLOC",""],["EXPECTED_NET_VIOLATIONS",""],["EXPECTED_TOTAL_VIOLATIONS",""]]});

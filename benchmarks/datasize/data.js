8dc5f535251725aee58d8a28a8c0cb84797c2e91
Manishearth <Manishearth@users.noreply.github.com>
2022-04-02 01:22:28 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@09cd22c35bdb00c19757d70309c4e756b4f941bf

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648857057183,
+  "lastUpdate": 1648861530825,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -12443,6 +12443,96 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/e265c6023a7fcad356632274177359e1ab9d5fa8"
         },
         "date": 1648857042412,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 4555035,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "579b7a52c50780af63a1f5fbd7b132fbd0ce7adc",
+          "message": "Support non-Ord values in ZeroMap (#1743)",
+          "timestamp": "2022-04-01T17:04:45-07:00",
+          "tree_id": "bea6e54a82bfe73d8b932bd1a536fed4c27a8706",
+          "url": "https://github.com/unicode-org/icu4x/commit/579b7a52c50780af63a1f5fbd7b132fbd0ce7adc"
+        },
+        "date": 1648857971164,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 4555035,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "552a1edd517b59c9b192ef36cd9ac1039e1d1f09",
+          "message": "Convert Unicode Keywords and Transform Fields to LiteMap (#1762)",
+          "timestamp": "2022-04-01T17:14:08-07:00",
+          "tree_id": "566f9b8661907e2586ac317e90f6f2c1b04bb88a",
+          "url": "https://github.com/unicode-org/icu4x/commit/552a1edd517b59c9b192ef36cd9ac1039e1d1f09"
+        },
+        "date": 1648858526360,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 4555035,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "09cd22c35bdb00c19757d70309c4e756b4f941bf",
+          "message": "Fixup t_with_ser error (#1766)",
+          "timestamp": "2022-04-01T18:04:01-07:00",
+          "tree_id": "76ec875b2c27cfee9f31ddb6a32f1a7513532142",
+          "url": "https://github.com/unicode-org/icu4x/commit/09cd22c35bdb00c19757d70309c4e756b4f941bf"
+        },
+        "date": 1648861514551,
         "tool": "ndjson",
         "benches": [
           {

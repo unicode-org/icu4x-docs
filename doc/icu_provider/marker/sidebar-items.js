e452d507c8257e4fbd1abf59c8ad8e7136fa8f6d
robertbastian <robertbastian@users.noreply.github.com>
2022-07-19 20:48:09 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@1876f493829f79cf48521e9739cce2fd81c17615

--- doc/icu_provider/marker/sidebar-items.js
+++ doc/icu_provider/marker/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["DataMarker","Trait marker for data structs. All types delivered by the data provider must be associated with something implementing this trait."],["ResourceMarker",""]]});
+initSidebarItems({"trait":[["DataMarker","Trait marker for data structs. All types delivered by the data provider must be associated with something implementing this trait."],["KeyedDataMarker",""]]});

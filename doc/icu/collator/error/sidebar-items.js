968d8373e062852e92d9dead1d0fa83eb92e1a8e
robertbastian <robertbastian@users.noreply.github.com>
2022-07-20 16:17:09 +0000

    Rust API docs + benchmark dashboards -> GH Pages unicode-org/icu4x@219e20faaeead57f6e81ac67e0a61af828c9a0ee

---
+++ doc/icu/collator/error/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["CollatorError","Error returned by the constructor of `Collator`"]]});
---
+++ doc/icu/normalizer/error/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["NormalizerError","Normalizer-specific error"]]});

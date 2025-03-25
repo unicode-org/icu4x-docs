1b52433c2da1e66c9fbe74411b277077e6a0f752
Elango Cheran <elango@unicode.org>
2025-03-25 12:39:22 -0700

    Add docstrings

--- tools/github-to-astro.ts
+++ tools/github-to-astro.ts
@@ -2,6 +2,9 @@ import fs from 'node:fs';
 import { parseArgs } from "node:util";
 import path from 'node:path';
 
+/**
+ * class to represent the values passed from the CLI through to the helper methods
+ */
 class Context {
   versionStr: string;
   sitePrefix: string;
@@ -19,6 +22,11 @@ class Context {
 // if this were hosted on Github pages,
 const URI_SITE_PREFIX = "/icu4x-docs";
 
+/**
+ * Return the string of the AstroJS markdown "frontmatter"
+ * @param foundTitle The page title string found from the H1 heading of the GH Markdown content
+ * @returns the string of the AstroJS markdown "frontmatter"
+ */
 function icu4xAstroMdFrontMatter(foundTitle: string) {
   let frontMatterStr = "---" + "\n"
     + "title: " + foundTitle + "\n"
@@ -26,6 +34,10 @@ function icu4xAstroMdFrontMatter(foundTitle: string) {
   return frontMatterStr;
 }
 
+/**
+ * Define rules of transformations to go from the Github Markdown to AstroJS.
+ * Rules are given as pairs of regex patterns for detection and their corresponding replacement strings.
+ */
 const ICU4X_MD_REPLACEMENTS: Array<{pattern: string | RegExp; replacement: string}> = [
   // fix the code fence syntax highlighting language identifers
   {pattern: /```console.*/g, replacement: "```shell"},
@@ -36,6 +48,16 @@ const ICU4X_MD_REPLACEMENTS: Array<{pattern: string | RegExp; replacement: strin
   {pattern: /^# .*/g, replacement: ""},
 ];
 
+/**
+ * Convert the Github Markdown file contents string into the string of the AstroJS Markdown file body,
+ * where "body" is the AstroJS Markdown content that comes after the front matter.
+ * Some transformations are stateless, and are thus defined in ICU4X_MD_REPLACEMENTS.
+ * Other transformations are file- and version-specific, and thus must be expressed via
+ * some amount of code, using the Context.
+ * @param body Github Markdown file full contents
+ * @param ctx
+ * @returns the string of the AstroJS Markdown file "body" that follows the front matter
+ */
 function transformMdBody(body: string, ctx: Context) {
   let replacementBody = body;
   for (let {pattern, replacement} of ICU4X_MD_REPLACEMENTS) {
@@ -52,7 +74,14 @@ function transformMdBody(body: string, ctx: Context) {
   return replacementBody;
 }
 
-function icu4xGfmToAstroMd(content: string, inFilePath: string, ctx: {versionStr: string, sitePrefix: string}) {
+/**
+ * Get the entire AstroJS Markdown file string, given the relevant info of the Github Markdown file.
+ * @param content String of Github Markdown file contents
+ * @param inFilePath Path string of input Github Markdown file
+ * @param ctx 
+ * @returns The AstroJS Markdown file contents string, including front matter and "body"
+ */
+function icu4xGfmToAstroMd(content: string, inFilePath: string, ctx: Context) {
   // JS regex appears to match (.*) only to the end of the line, and JS regex does
   // not behave well when putting the EOL $ symbol at the end of the regex.
   const titleHeadingRegexMatch = /^# (.*)/.exec(content);
@@ -65,10 +94,22 @@ function icu4xGfmToAstroMd(content: string, inFilePath: string, ctx: {versionStr
   return frontMatter + "\n" + replacementContent;
 }
 
+/**
+ * Convert a semver string into a string necessary for URIs within the URL
+ * @param version Semantic version string
+ * @returns The semantic version string, with dots replaced with underscores
+ */
 function getUriVersionStr(version: string) {
   return version.replace(/\./g, "_");
 }
 
+/**
+ * Read an input GH Markdown file and write the transformed AstroJS content to the output file
+ * @param inFilePath String of input file path
+ * @param outFilePath String of output file path
+ * @param ctx 
+ * @returns Return the string of the transformed Markdown appropriate for AstroJS
+ */
 function readConvertWriteFile(inFilePath: string, outFilePath: string, ctx: Context) {
   let data: string = "";
 
@@ -91,6 +132,9 @@ function readConvertWriteFile(inFilePath: string, outFilePath: string, ctx: Cont
   return;
 }
 
+/**
+ * Print CLI usage
+ */
 function printHelp() {
   console.log("Convert ICU4X Github repo Markdown tutorials to Astro MDX files");
   console.log();
@@ -98,6 +142,10 @@ function printHelp() {
   console.log("\tnpx tsx -- --inFile=<input-file> --outFile=<output-file> --icu4xTag=<ICU4X-semver> --sitePrefx=<site-prefix-str-else-emptystr>");
 }
 
+/**
+ * Parse CLI args
+ * @returns Parsed CLI structure per API
+ */
 function parseCLIArgs() {
   let parsedArgs = parseArgs({
     options: {
@@ -139,6 +187,8 @@ function parseCLIArgs() {
     process.exit(1);
   }
 }
+
+// "main"
 
 try {
   const parsedArgs = parseCLIArgs();

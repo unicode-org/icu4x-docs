7795601a22639683d3c546231fe294ff7ca9fe6a
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2025-05-19 12:17:37 +0200

    Redirect version root to quickstart, latest to latest (#41)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import starlightLinksValidator from 'starlight-links-validator'
 
 
 // !! UPDATE LATEST VERSION HERE !!
-let latest_version = '2.0';
+const latest_version = '2.0';
 
-let latest_dir_name = latest_version.replace('.', '_');
+export const latest_dir_name = latest_version.replace('.', '_');
 
 // https://astro.build/config
 export default defineConfig({

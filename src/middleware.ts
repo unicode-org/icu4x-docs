26db7c54fabba70fe55eebfde8e41d5aad9a0ec2
Robert Bastian <robertbastian@unicode.org>
2025-05-19 12:16:53 +0200

    more

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,20 @@
-import { defineMiddleware, sequence } from "astro/middleware";
+import { sequence } from "astro/middleware";
+import { latest_dir_name } from "../astro.config.mjs";
 
-// path uses a semver (2.0.1) instead of a directory name (2_0)
-export const onRequest = sequence(redirectSemver, redirectVersionRoot);
+export const onRequest = sequence(redirectLatest, redirectSemver, redirectVersionRoot, redirectTutorialsRoot);
 
+// redirect /latest to the latest version
+function redirectLatest(context, next) {
+  if (context.url.pathname.slice(0, 7) == '/latest') {
+    let rest = context.url.pathname.slice(context.url.pathname.length == 7 ? 7 : 8);
+    return Response.redirect(new URL(`/${latest_dir_name}/${rest}`, context.url), 302);
+
+  }
+
+  return next();
+}
+
+// redirect a semver (/2.0.1) to a directory name (/2_0)
 function redirectSemver(context, next) {
   let parts = context.url.pathname.match(/^\/(?<major>[0-9]+)\.(?<minor>[0-9])+\.[0-9]+(?<rest>\/.*)?$/);
   if (parts?.groups) {
@@ -12,10 +24,20 @@ function redirectSemver(context, next) {
   return next();
 }
 
-
-// redirect the version root (/2_0/) to quickstart (/2_0/tutorials/quickstart/)
+// redirect the version root (/2_0) to quickstart (/2_0/tutorials/quickstart)
 function redirectVersionRoot(context, next) {
   let parts = context.url.pathname.match(/^\/(?<major>[0-9]+)_(?<minor>[0-9])+\/?$/);
+  if (parts?.groups) {
+    return Response.redirect(new URL(`/${parts.groups['major']}_${parts.groups['minor']}/tutorials/quickstart`, context.url), 302);
+  }
+
+  return next();
+};
+
+
+// redirect the tutorials root (/2_0/tutorials) to quickstart (/2_0/tutorials/quickstart)
+function redirectTutorialsRoot(context, next) {
+  let parts = context.url.pathname.match(/^\/(?<major>[0-9]+)_(?<minor>[0-9])+\/tutorials\/?$/);
   if (parts?.groups) {
     return Response.redirect(new URL(`/${parts.groups['major']}_${parts.groups['minor']}/tutorials/quickstart`, context.url), 302);
   }
